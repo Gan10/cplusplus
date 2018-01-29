76e940c90a740d787bb2c182fb1736a62017658d
Gan10 <ganesha.srithar@gmail.com>
2018-01-29 10:00:49 +0100

    exo 1 fini

--- include/my_header.hpp
+++ include/my_header.hpp
@@ -4,7 +4,15 @@
 #ifndef MY_HEADER_HPP
 #define MY_HEADER_HPP
 
+double exo1 (double rayon)
+{
+	double volume;
+	
+	volume = 4.0/3.0  * 3.14 * rayon * rayon * rayon;
+	
+	return volume;
 
+}
 
 #endif // MY_HEADER_HPP
 
--- source/main.cpp
+++ source/main.cpp
@@ -6,8 +6,11 @@
 
 int main()
 {
-    // Affichage sur un terminal
-    std::cout << "Hello World!" << std::endl;
-
-    return 0;
+	double r1;
+	r1 = exo1(10.0);
+	// Affichage sur un terminal
+	std::cout << "Hello World!" << std::endl;
+    
+	return 0;
+	
 }
